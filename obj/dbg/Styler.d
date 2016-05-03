f9a41aac2f928fcb7b7df319e53eb1bf34830d8f
Stephen Ashmore <StephenCAshmore@gmail.com>
2016-05-02 23:26:54 -0500

    Removed object folder.

--- obj/dbg/Styler.d
+++
@@ -1,2 +0,0 @@
-../obj/dbg/Styler.o: Styler.cpp Styler.h global.h
-	g++ -Wall -Werror -Wshadow -pedantic -I/usr/local/include -I../../../src -std=gnu++11 -g -D_DEBUG -c Styler.cpp -o ../obj/dbg/Styler.o
--- obj/opt/Styler.d
+++
@@ -1,2 +0,0 @@
-../obj/opt/Styler.o: Styler.cpp Styler.h global.h
-	g++ -Wall -Werror -Wshadow -pedantic -I/usr/local/include -I../../../src -std=gnu++11 -O3 -c Styler.cpp -o ../obj/opt/Styler.o
